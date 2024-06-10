2de0ea35ac0c88e11ccd92e90abbb86dec75832a
AGBOTA-Stevens <agbotagu@gmail.com>
2024-06-10 12:21:22 +0100

    modifié :         SoulQuest/__pycache__/__init__.cpython-310.pyc 	modifié :         SoulQuest/__pycache__/settings.cpython-310.pyc 	modifié :         SoulQuest/__pycache__/urls.cpython-310.pyc 	modifié :         SoulQuest/__pycache__/wsgi.cpython-310.pyc 	modifié :         SoulQuest/settings.py 	modifié :         Utilisateurs/__pycache__/__init__.cpython-310.pyc 	modifié :         Utilisateurs/__pycache__/admin.cpython-310.pyc 	modifié :         Utilisateurs/__pycache__/apps.cpython-310.pyc 	modifié :         Utilisateurs/__pycache__/models.cpython-310.pyc 	modifié :         Utilisateurs/__pycache__/urls.cpython-310.pyc 	modifié :         Utilisateurs/__pycache__/views.cpython-310.pyc 	nouveau fichier : Utilisateurs/migrations/0001_initial.py 	modifié :         Utilisateurs/migrations/__pycache__/__init__.cpython-310.pyc 	modifié :         Utilisateurs/models.py 	modifié :         compte/__pycache__/__init__.cpython-310.pyc 	modifié :         compte/__pycache__/admin.cpython-310.pyc 	modifié :         compte/__pycache__/apps.cpython-310.pyc 	modifié :         compte/__pycache__/forms.cpython-310.pyc 	modifié :         compte/__pycache__/models.cpython-310.pyc 	modifié :         compte/__pycache__/urls.cpython-310.pyc 	modifié :         compte/__pycache__/views.cpython-310.pyc 	modifié :         compte/migrations/__pycache__/__init__.cpython-310.pyc 	modifié :         compte/urls.py 	nouveau fichier : media/media_files/Mashle_OP2_Lyrics__Bling-Bang-Bang.mp4 	nouveau fichier : media/media_files/Thriller.mp4 	nouveau fichier : media/media_files/imagine_dragons_demo..._27704.mp4 	nouveau fichier : media/profile_pics/gerod.jpg 	nouveau fichier : media/profile_pics/harus.jpg 	nouveau fichier : media/profile_pics/mariline.jpg 	supprimé :        media/media_files/Mashle_OP2_Lyrics__Bling-Bang-Bang.mp4 	supprimé :        media/media_files/Thriller.mp4 	supprimé :        media/media_files/imagine_dragons_demo..._27704.mp4 	supprimé :        media/profile_pics/gerod.jpg 	supprimé :        media/profile_pics/harus.jpg 	supprimé :        media/profile_pics/mariline.jpg 	Utilisateurs/migrations/__pycache__/0001_initial.cpython-310.pyc

--- Utilisateurs/models.py
+++ Utilisateurs/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+#La table des Utilisateurs 
+class Zz_Users(models.Model):
+    zz_email = models.EmailField(unique=True, max_length=100)
+    zz_password = models.CharField(max_length=100)
+
+
+class Zz_MediaFile(models.Model):
+    zz_user = models.ForeignKey(Zz_Users, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='media_files/')
+    # la table des médias ou vidéos...
+
+    
+
+
+
+
+class Zz_profile(models.Model):
+    zz_user = models.OneToOneField(Zz_Users, on_delete=models.CASCADE)
+    zz_name = models.CharField(unique=True, max_length=100)
+    zz_prenom =models.CharField(max_length=100)
+
+    zz_date_naissance = models.DateField(unique=False)
+    
+    zz_sexe = models.CharField(max_length=255, choices=[('M', 'Masculin'), ('F', 'Féminin')], unique=False)
+    
+    zz_recherche = models.CharField(max_length=255, choices=[(0, 'homme'), (1, 'Femme'), (None, 'les deux')], unique=False)
+    
+    zz_profile_picture = models.ImageField(upload_to='profile_pics/')
+    
+    zz_additional_media = models.ManyToManyField(Zz_MediaFile, blank=True)
+
--- compte/urls.py
+++ compte/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/inscription', views.InscriptionPage, name='inscription'),
+    path('inscription/', views.InscriptionPage, name='inscription'),
     path('', views.ConnexionPage, name='connexion'),
 ]
