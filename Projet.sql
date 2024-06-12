7cacf16ffb39ed5dc6c1dd91c90a4eec05b34bce
FirStone007 <mlogstone@gmail.com>
2024-06-12 09:44:38 +0100

    pyc files

--- Projet.sql
+++ Projet.sql
@@ -1,8 +1,8 @@
 -- Adminer 4.8.1 MySQL 8.2.0 dump
-
+USE ProjectDB;
 SET NAMES utf8;
 SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
+SET foreign_key_checks = 0; 
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP TABLE IF EXISTS `zz_astrales`;
