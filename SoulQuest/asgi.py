bfc2c222ead19e73083b8cadfa0ad9ffd39ce08b
FirStone007 <mlogstone@gmail.com>
2024-06-14 20:36:36 +0100

    database upgrading

--- SoulQuest/asgi.py
+++ SoulQuest/asgi.py
@@ -10,7 +10,19 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SoulQuest.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+from messagerie import routing
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+    ),
+    # Just HTTP for now. (We can add other protocols later.)
+})
